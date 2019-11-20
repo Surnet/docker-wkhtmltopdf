ab8afc2a1948429f2bf9c699477d9e35be03b88b
chdanielmueller <daniel.mueller@surnet.ch>
2019-11-20 22:26:19 +0100

    node:12.13.0-alpine3.10

--- update.sh
+++ update.sh
@@ -36,7 +36,7 @@ for version in \
     # Supported base images
     for image in \
       alpine:3.10 \
-      node:10.16.0-alpine \
+      node:12.13.0-alpine3.10 \
       python:3.7.3-alpine3.10 \
     ; do
       # Parse image string
